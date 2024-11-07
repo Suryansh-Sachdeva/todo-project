0020d3a43c840f81f0ce82e70b1d8f0d01090d7e
Suryansh-Sachdeva <mr.suryansh08@gmail.com>
2024-11-07 19:11:33 +0300

    project going on

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-txt = str(input("Choose between Add, Delete and Change: "))
+txt = str(input("Choose between 'Add', 'Delete' and 'Change': "))
+if txt == "Add":
+    add_content = str(input("Enter the command to add: "))
+    f = open("Commands.txt", "a")
+    f.write(add_content + "\n")
+    f.close()
