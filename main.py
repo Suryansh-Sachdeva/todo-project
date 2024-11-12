98d291a120a33b3916c0f30c6b3cb180b23e3a1c
Suryansh-Sachdeva <mr.suryansh08@gmail.com>
2024-11-12 19:10:09 +0300

    Project ongoing

--- main.py
+++ main.py
@@ -4,3 +4,18 @@ if txt == "Add":
     f = open("Commands.txt", "a")
     f.write(add_content + "\n")
     f.close()
+if txt == "Delete":
+    def delete_line(filename, line_number):
+        with open(filename) as file:
+            lines = file.readlines()
+        if (line_number <= len(lines)):
+            del lines[line_number - 1]
+            with open(filename, "w") as file:
+                for line in lines:
+                    file.write(line)
+        else:
+            print("Line", line_number, "not in file.", end=" ")
+            print("File has", len(lines), "lines.")
+    delete_filename = "Commands.txt"
+    delete_file_number = int(input("Line number: "))
+    delete_line(delete_filename, delete_file_number)
