f521c8ccda41e87f79ba212e83907a8065069548
Suryansh-Sachdeva <mr.suryansh08@gmail.com>
2024-11-26 15:28:47 +0300

    commit

--- main.py
+++ main.py
@@ -22,4 +22,22 @@ if txt == "Delete":
     delete_filename = "Commands.txt"
     delete_file_number = int(input("Line number: "))
     delete_line(delete_filename, delete_file_number)
-    
+if txt == "Change":
+    def replace_line(filename, line_number, text):
+        with open(filename) as file:
+            lines = file.readlines()
+        if (line_number <= len(lines)):
+            lines[line_number - 1] = text + "\n"
+            with open(filename, "w") as file:
+                for line in lines:
+                    file.write(line)
+        elif len(lines) == 1:
+            print("Line", line_number, "not in file.", end=" ")
+            print("File has 1 line.")
+        else:
+            print("Line", line_number, "not in file.", end=" ")
+            print("File has", len(lines), "lines.")
+    filename = "Commands.txt"
+    line_number = int(input("Line number: "))
+    text = input("Text: ")
+    replace_line(filename, line_number, text)
